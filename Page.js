6c331e0240cf44911665c2828ed31674221f8b88
eveah-codes <52373568+eveah-codes@users.noreply.github.com>
2023-11-18 17:15:44 +0000

    Updated router.js and added a page.js file, Single page application routing completed

---
+++ Page.js
@@ -0,0 +1,20 @@
+const page = (pageDetails) => {
+    const section = document.createElement("section")
+    section.innerHTML = `
+        <h1>${pageDetails.greet}</h1>
+        <p>${pageDetails.subText}</p>
+        <img src= ${pageDetails.src} alt="a beautiful woman"/>
+    `
+    return section
+
+}
+const homePage = page({ greet: "Hello, welcome to my Portfolio", subText: "My name is Ella, I am a graphic design artist", src: "https://cdn.pixabay.com/photo/2015/07/22/21/25/woman-856125_640.jpg" })
+
+const aboutPage = page({ greet: "Get to know me better", subText: "Hey there, I'm a passionate graphic design artist with a fervent love for visual storytelling. I dive into the world of design with an aim to create captivating narratives that resonate. For me, design isn't just about colors and shapes; it's about emotions, connections, and the magic of transforming ideas into something tangible.", src: "https://cdn.pixabay.com/photo/2023/07/25/18/42/vector-graphic-8149677_640.jpg" })
+
+const contactPage = page({ greet: "Let's talk 👇🏾", subText: "I would love to hear from you, you can send an email to ella@test.com", src: "" })
+
+
+
+export {homePage, aboutPage, contactPage}
+
--- Router.js
+++ Router.js
@@ -1,52 +1,52 @@
-//create a router object to handle navigation in the app. It has two methods, init and go.
-const Router = {
-    //init is a method used to add click event listeners to each navlink. then it uses event.preventDefault to prevent the default page refresh. init then gets the current url by using the current link getAttribute("href") method. it saves this url in a constant called url. finnaly it passes this url as an argument to the second router object method go i.e Router.go(url)
-    init: () => {
-        document.querySelectorAll("a.navlink").forEach(link => {
-            link.addEventListener("click", event => {
-                event.preventDefault()
-                const url = link.getAttribute("href")
-                Router.go(url)
 
+import { aboutPage, contactPage, homePage } from "./Page.js";
+
+const Router = {
+    init: () => {
+        //set up event listener for nav links
+        const navLinks = document.querySelectorAll(".nav__link");
+        navLinks.forEach(link => {
+            link.addEventListener("click", (event) => {
+                event.preventDefault()
+                const url = link.querySelector("a").getAttribute("href")
+                //by the second arg is addToHistory == true
+                Router.go(url)
             })
         })
-        //this event listener updates the page content when the url is changed by listening for the popstate event.
-        window.addEventListener("popstate", event => {
-            //calls the go method of the router object and passes the v
+        //listen for page navigation
+        window.addEventListener("popstate", (event) => {
             Router.go(event.state.route, false)
+            
         })
-        //check the initial URL
-        Router.go(location.pathname)
     },
     go: (route, addToHistory = true) => {
+        //update the browser history with a new route
         if (addToHistory) {
             history.pushState({ route }, null, route)
         }
         let pageElement = null
+        const main = document.querySelector(".main")
+        //navigate to a given route and update the page content based on the route
         switch (route) {
             case "/":
-                pageElement = document.createElement("h1")
-                pageElement.textContent = "Menu"
+                pageElement = homePage
                 break;
-            case "/order":
-                pageElement = document.createElement("h1")
-                pageElement.textContent = "Your Order"
+            case "/about":
+                pageElement = aboutPage
+                break;
+            case "/contact":
+                pageElement = contactPage
                 break;
             default:
-                if (route.startsWith("/product-")) {
-                    pageElement = document.createElement("h1")
-                    pageElement.textContent = "Details"
-                    const paramId = route.subString(route.lastIndexOf("-") + 1)
-                    pageElement.dataset.id = paramId
-                }
                 break;
         }
+
         if (pageElement) {
-            const cache = document.querySelector("main")
-            if (Array.from(cache.children).length > 0) {
-                cache.children[0].remove()
+            if (Array.from(main.children).length > 0) {
+                main.children[0].remove()
             }
-            cache.appendChild(pageElement)
+            main.appendChild(pageElement)
+            //when a new page is loaded, the viewport starts at the very top of the page without any prior scroll position
             window.scrollX = 0;
             window.scrollY = 0;
         }
